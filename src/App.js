4ae95be98a383b0f70bb33d5ea8a86b68bc3aee7
Arystan <arystankaliakparov@gmail.com>
2022-10-31 18:28:53 +0600

    admin app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Posts from "./pages/posts/Posts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="ui container">
+        <div className="ui menu">
+          <div className="header item">
+            Blog Admin
+          </div>
+        </div>
+        <Routes>
+          <Route path="/" element={<Posts />} />
+          <Route path="/posts">
+            <Route index element={<Posts />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/posts/Posts.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import client from "../../client";
+
+function Posts() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const loadPosts = () => {
+    setLoading(true);
+    client.get("/posts")
+      .then((res) => setPosts(res.data))
+      .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    loadPosts()
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="ui text active loader">Loading</div>
+    )
+  }
+
+  return (
+    <>
+      <Link to={`/posts/new`}>
+        <button className="ui primary basic button">New Post</button>
+      </Link>
+      <table className="ui loading celled table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Created At</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post._id}>
+              <td data-label="Title">{post.title}</td>
+              <td data-label="Created At">{post.createdAt}</td>
+              <td data-label="Actions"></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Posts
