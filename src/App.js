81983b5be6a697610e5369c1684b064f6675fff6
theuniking <parolpassword3>
2022-11-07 20:33:31 +0600

    disable

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx a11y/anchor-is-valid */
 import React from "react";
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Posts from "./pages/posts/Posts";
--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const client  = axios.create({ baseURL: 'http://localhost:3004' })
+const client  = axios.create({ baseURL: 'https://blog-server-universe.herokuapp.com' })
 
 client.interceptors.request.use((config) => {
     const token = localStorage.getItem('token')
